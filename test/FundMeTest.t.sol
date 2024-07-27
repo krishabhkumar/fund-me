1d66890e86e64166b9c50074c887ec4948960238
Rishabh <rishabhk.ip.20@nitj.ac.in>
2024-07-27 22:15:58 +0530

    first commit

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+
+        FundMe public fundMe;
+
+        function setUp() external {
+            fundMe = new FundMe();
+        }
+
+        function testMinimumDollarIsFive() public view {
+            assertEq(fundMe.MINIMUM_USD(), 5e18);
+        }
+
+        function testOwnerIsMsgSender() public view {
+            assertEq(fundMe.i_owner(), address(this));
+        }
+
+        // This test is not done satisfactorily
+        
+        function testPriceFeedVersionIsAccurate() public {
+            uint version = fundMe.getVersion();
+            assertEq(version, 4);
+        }
+    
+}
